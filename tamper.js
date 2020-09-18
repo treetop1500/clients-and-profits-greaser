0441134152cfce08e3326dc87b9cfa5a04dd6a2f
Robert Wade <rwade@grayloon.com>
2020-09-18 12:35:34 -0500

    Update tamper.js
    updates the version number

--- tamper.js
+++ tamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://grayloon.jobr.mobi/index.php
